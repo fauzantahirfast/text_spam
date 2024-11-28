a0f5af78aadb3fe72297dff5651cc7b6f31dfc14
Fauzan <fauzantahir660@gmail.com>
2024-11-28 12:07:46 +0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import "./css/app.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [input, setInput] = useState("")
+    const [response, setResponse] = useState("")
+    const sendData = async () => {
+        const data = input
+        try {
+            const response = await fetch('/api/data', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+
+            const result = await response.json();
+            setResponse(result)
+            console.log('Response from Flask:', result);
+        } catch (error) {
+            console.error('Error sending data:', error);
+        }
+    }
+    return (
+        <>
+            <div className='container-fluid p-0'>
+                <nav className="navbar navbar-dark bg-dark p-2">
+                    <a className="navbar-brand" href="#">Email Spam Classifier</a>
+                </nav>
+
+                <div className="container mt-5">
+                    <div className="row justify-content-start fs-1">
+                        <div className="col-6">
+                            <div className="text-black" style={{ fontWeight: "600" }}>Email Spam Checker</div>
+                        </div>
+                    </div>
+                    <div className="row justify-content-start fs-4 mt-3">
+                        <div className="col-6">
+                            <textarea type="text" className='w-100' onChange={(e)=> setInput(e.target.value)}/>
+                        </div>
+					</div>
+					<div className="row justify-content-start fs-5 mt-3">
+                        <div className="col-2" >
+                            <button className='bg-dark text-white rounded' onClick={sendData}>Submit</button>
+                        </div>
+                    </div>
+                    <div className="row justify-content-start fs-5 mt-3">
+                        <div className="col-2" >
+                            <h4 className='text-dark'>{response["Classified data"]}</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
 
 export default App;
